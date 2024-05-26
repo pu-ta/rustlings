b9c883e8090740f88f6840668eee08f488d15b55
pu-ta <166606453+pu-ta@users.noreply.github.com>
2024-05-26 15:42:05 +0900

    move_semantics1,2,3,4,5,6

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint move_semantics1` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn main() {
     let vec0 = vec![22, 44, 66];
@@ -12,10 +10,13 @@ fn main() {
     let vec1 = fill_vec(vec0);
 
     assert_eq!(vec1, vec![22, 44, 66, 88]);
+    // for e in &vec0 {
+    //     println!("{e}")
+    // }
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let vec = vec;
+    let mut vec = vec;
 
     vec.push(88);
 
